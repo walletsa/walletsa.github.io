8972c5add7f356fa9fac6138185a13e0a27b7249
walletsa <goinghigh1809@gmail.com>
2026-09-10 11:04:14 +0800

    Patch verified images and add missing Shin-chan cards

---
+++ card-collector/data-5.js
@@ -0,0 +1,32 @@
+(()=>{
+const P={
+"icash-shin-002":{image:"https://cdn-origin.cool-style.com.tw/cool/2025/04/icash_2025041.jpg",imageSource:"正確商品圖",verified:"verified",source:"https://www.icash.com.tw/Home/NewsDetail/?AspxAutoDetectCookieSupport=1&ID=12335"},
+"icash-shin-006":{image:"https://i.beauty321.com/600x/https%3A/il.beauty321.com/gallery/gallery/62327/photo-6670ee8c5b16e.jpg",imageSource:"正確商品圖",verified:"verified",source:"https://www.icash.com.tw/Home/NewsDetail/?AspxAutoDetectCookieSupport=1&ID=11906"},
+"icash-shin-012":{image:"https://img.91app.com/webapi/imagesV3/Original/SalePage/9195773/0/639105984376830000?v=1",imageSource:"正確商品圖",verified:"verified"},
+"icash-shin-013":{image:"https://img.pchome.com.tw/cs/items/DEAS93A900ITVRF/000001_1747389358.jpg",imageSource:"正確商品圖",verified:"verified"},
+"icash-shin-014":{image:"https://shopping.line-scdn.net/0h0Wq4eTIEb3hMTXwEuCIQLxQQcwkkOzZvM3V1WjsILxo6fG1SK0hldSI2MiBidWlSLFR8YhsmeikHPG5RKEAhfw8tbR0EfGFGK0whdRwqcSBiAWNFc3lmZTUXOTQR",imageSource:"正確商品圖",verified:"verified"},
+"icash-shin-016":{image:"https://p2.bahamut.com.tw/B/2KU/98/80de059d8306d416fa053112151j9ky5.JPG",imageSource:"正確商品圖",verified:"verified",source:"https://www.icash.com.tw/Home/NewsDetail/?AspxAutoDetectCookieSupport=1&ID=11157"},
+"icash-shin-017":{image:"https://down-tw.img.susercontent.com/file/tw-11134207-81ztl-mez9e0ef06j3df",imageSource:"正確系列合圖",verified:"verified",source:"https://www.icash.com.tw/Home/NewsDetail/?AspxAutoDetectCookieSupport=1&ID=10972"},
+"icash-shin-018":{image:"https://down-tw.img.susercontent.com/file/tw-11134207-81ztl-mez9e0ef06j3df",imageSource:"正確系列合圖",verified:"verified",source:"https://www.icash.com.tw/Home/NewsDetail/?AspxAutoDetectCookieSupport=1&ID=10972"},
+"icash-shin-019":{image:"https://down-tw.img.susercontent.com/file/tw-11134207-81ztl-mez9e0ef06j3df",imageSource:"正確系列合圖",verified:"verified",source:"https://www.icash.com.tw/Home/NewsDetail/?AspxAutoDetectCookieSupport=1&ID=10972"},
+"icash-shin-020":{image:"https://down-tw.img.susercontent.com/file/tw-11134207-81ztl-mez9e0ef06j3df",imageSource:"正確系列合圖",verified:"verified",source:"https://www.icash.com.tw/Home/NewsDetail/?AspxAutoDetectCookieSupport=1&ID=10972"},
+"icash-shin-021":{image:"https://down-tw.img.susercontent.com/file/tw-11134207-81ztd-mej7slih92wxc6",imageSource:"正確系列合圖",verified:"verified"},
+"icash-shin-022":{image:"https://down-tw.img.susercontent.com/file/tw-11134207-81ztd-mej7slih92wxc6",imageSource:"正確系列合圖",verified:"verified"},
+"icash-shin-023":{image:"https://down-tw.img.susercontent.com/file/tw-11134207-81ztd-mej7slih92wxc6",imageSource:"正確系列合圖",verified:"verified"},
+"icash-shin-024":{image:"https://down-tw.img.susercontent.com/file/tw-11134207-81ztd-mej7slih92wxc6",imageSource:"正確系列合圖",verified:"verified"}
+};
+(window.CARD_DATA||[]).forEach(x=>{if(P[x.id])Object.assign(x,P[x.id]);});
+const extra=[
+{id:"easy-shin-extra-giant-001",ip:"蠟筆小新",issuer:"悠遊卡",date:"2024-06-28",name:"蠟筆小新SuperCard巨大造型悠遊卡",image:"https://down-tw.img.susercontent.com/file/tw-11134207-7r98u-lx920mccg0fk92",verified:"verified",imageSource:"正確商品實拍",source:"https://www.easycard.com.tw/museum?keywords=%E8%A0%9F%E7%AD%86%E5%B0%8F%E6%96%B0"},
+{id:"icash-shin-extra-001",ip:"蠟筆小新",issuer:"icash",date:"2024-03-05",name:"蠟筆小新-動感超人迷你背包 icash2.0",image:"https://cc.tvbs.com.tw/img/upload/2024/03/02/20240302105501-200959e3.jpg",verified:"verified",imageSource:"正確商品圖",source:"https://www.icash.com.tw/Home/NewsDetail/?ID=11758"},
+{id:"icash-shin-extra-002",ip:"蠟筆小新",issuer:"icash",date:"2023-10-17",name:"蠟筆小新動感超人公仔 icash2.0",image:"https://img.chilling.tw/images/author/Ivy/003%20-%202023-10-12T160337_136.jpg",verified:"verified",imageSource:"正確商品圖",source:"https://www.icash.com.tw/Home/NewsDetail/?ID=11551"},
+{id:"icash-shin-extra-003",ip:"蠟筆小新",issuer:"icash",date:"2023-01-17",name:"蠟筆小新-妮妮的兔子 icash2.0",image:"",verified:"official",imageSource:"待補正確圖片",source:"https://www.icash.com.tw/Home/NewsDetail/?ID=11218"},
+{id:"icash-shin-extra-004",ip:"蠟筆小新",issuer:"icash",date:"2022-07-12",name:"蠟筆小新變裝系列-搭巴士 icash2.0",image:"",verified:"official",imageSource:"待補正確圖片",source:"https://www.icash.com.tw/Home/NewsDetail/?AspxAutoDetectCookieSupport=1&ID=11048"},
+{id:"icash-shin-extra-005",ip:"蠟筆小新",issuer:"icash",date:"2022-07-12",name:"蠟筆小新變裝系列-躲被被睡覺 icash2.0",image:"",verified:"official",imageSource:"待補正確圖片",source:"https://www.icash.com.tw/Home/NewsDetail/?AspxAutoDetectCookieSupport=1&ID=11048"},
+{id:"icash-shin-extra-006",ip:"蠟筆小新",issuer:"icash",date:"2021-12-29",name:"蠟筆小新-臉紅公仔 icash2.0",image:"",verified:"official",imageSource:"待補正確圖片",source:"https://www.icash.com.tw/Home/CardList?AspxAutoDetectCookieSupport=1&type=10&year=2021"},
+{id:"icash-shin-extra-007",ip:"蠟筆小新",issuer:"icash",date:"2020-04-08",name:"蠟筆小新-睡衣版 icash2.0",image:"",verified:"official",imageSource:"待補正確圖片",source:"https://www.icash.com.tw/Home/NewsDetail/?ID=10135"},
+{id:"icash-shin-extra-008",ip:"蠟筆小新",issuer:"icash",date:"2020-04-08",name:"蠟筆小新-動感超人 icash2.0",image:"",verified:"official",imageSource:"待補正確圖片",source:"https://www.icash.com.tw/Home/NewsDetail/?ID=10135"}
+];
+const ids=new Set((window.CARD_DATA||[]).map(x=>x.id));
+window.CARD_DATA=(window.CARD_DATA||[]).concat(extra.filter(x=>!ids.has(x.id)));
+})();
